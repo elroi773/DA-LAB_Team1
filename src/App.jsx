8091b17eb05d81088f1565bffe495763540d22b1
elroi773 <s2457@e-mirim.hs.kr>
2025-11-20 20:26:10 +0900

    라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import LookBook from './pages/LookBook'
+import GroupCreate from './pages/GroupCreate'
+import GiverMain from './pages/GiverMain'
 
 
 function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/main" element={<SpaceMain />} /> */}
-    </Routes>
+        <Route path="/look-book" element={<LookBook />} />
+        <Route path="/groupcreate" element={<GroupCreate />}/>
+        <Route path="/" element={<GiverMain />}/>
+      </Routes>
     </>
   )
 }
