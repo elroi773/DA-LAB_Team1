ecb2032456d15e4e6c79532cff706adf627b9650
elroi773 <s2457@e-mirim.hs.kr>
2025-11-20 19:03:25 +0900

    초기 세팅 완료

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        {/* <Route path="/main" element={<SpaceMain />} /> */}
+    </Routes>
+    </>
+  )
+}
+
+export default App
