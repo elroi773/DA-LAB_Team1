34d2436a84bd2b9cd25d1866ca534bbbb265a8e6
Woojiyeong <a01086824402@gmail.com>
2025-11-22 07:42:08 +0900

    Receiver 메인 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import LookBook from './pages/LookBook'
-import GroupCreate from './pages/GroupCreate'
-import GiverMain from './pages/GiverMain'
-
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ReceiverMain from "./pages/Receiver_main";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/look-book" element={<LookBook />} />
-        <Route path="/groupcreate" element={<GroupCreate />}/>
-        <Route path="/" element={<GiverMain />}/>
+        <Route path="/receiver" element={<ReceiverMain />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
