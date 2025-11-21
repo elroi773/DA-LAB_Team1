add2d45678d25bd6537088640e679e3b3012106e
Woojiyeong <a01086824402@gmail.com>
2025-11-22 06:34:26 +0900

    칭찬도장 받은 후 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-
+// import Before from './component/Before_Ui'
+import Group from './component/Group'
 
 function App() {
   return (
     <>
+    <Group></Group>
+    {/* <Before></Before> */}
     </>
   )
 }
