a57ca799228b698802859cbea43d5addbbee8e10
Woojiyeong <a01086824402@gmail.com>
2025-11-21 23:09:49 +0900

    Receiver pages Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import LookBook from './pages/LookBook'
-import GroupCreate from './pages/GroupCreate'
-import GiverMain from './pages/GiverMain'
 
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/look-book" element={<LookBook />} />
-        <Route path="/groupcreate" element={<GroupCreate />}/>
-        <Route path="/" element={<GiverMain />}/>
-      </Routes>
     </>
   )
 }
