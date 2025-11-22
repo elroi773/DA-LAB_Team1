0d726171e7a1de4b8141109f0796e1730d755e10
elroi773 <s2457@e-mirim.hs.kr>
2025-11-22 22:43:42 +0900

    router

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import { Routes, Route } from 'react-router-dom'
 // import Before from './component/Before_Ui'
 import Group from './component/Group'
 import ReceiverMain from "./pages/Receiver_main";
+import Login from "./pages/Login/Login";
+import SignUp from './pages/SignUp/SignUp';
+import ReceiverMain from "./pages/Receiver_main";
+import SpaceMain from './pages/SpaceMain';
 
 function App() {
   return (
     <>
-    <Group></Group>
-    {/* <Before></Before> */}
       <Routes>
         <Route path="/receiver" element={<ReceiverMain />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/receiver" element={<ReceiverMain />} />
+        <Route path="/space" element={<SpaceMain />} />
+        
       </Routes>
     </>
   );
