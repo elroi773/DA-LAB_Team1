d84c2a0011268fc3197edb91c0267be5fbd210b3
elroi773 <s2457@e-mirim.hs.kr>
2025-11-23 18:02:27 +0900

    그룹 생서 ㅇ연동 backend

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-const supabaseSecretKey = import.meta.env.VITE_SUPABASE_SECRET_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
-export const supabaseAdmin = createClient(supabaseUrl, supabaseSecretKey)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    }
+  }
+);
