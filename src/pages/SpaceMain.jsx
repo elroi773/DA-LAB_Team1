cae60c9b3f43973863662129d55cbb3db07ba88d
김이레 <165885725+elroi773@users.noreply.github.com>
2025-11-24 12:55:03 +0900

    Update SpaceMain.jsx

--- src/pages/SpaceMain.jsx
+++ src/pages/SpaceMain.jsx
@@ -106,7 +106,7 @@ export default function SpaceMain() {
 
         <div css={group2}>
           <button css={groupBtn} onClick={goToGiverMain}>
-            그룹 생성하기
+            그룹 관리하기 
           </button>
           <button onClick={goToReceiverMain} css={groupBtn}>
             그룹 입장하기
