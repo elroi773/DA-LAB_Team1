eb0d55e369639d2eb5d2a60d2a1fe001d2ca9a9a
elroi773 <s2457@e-mirim.hs.kr>
2025-12-05 19:45:07 +0900

    윈도우, 맥 메시지 안보임 문제 해결, 파리미터 문제 해결

--- src/api/Hearts.jsx
+++ src/api/Hearts.jsx
@@ -4,14 +4,20 @@ import {supabase} from './supabaseClient.js'
 
 // 하트 주기 (합계는 COUNT or SUM으로 계산)
 export async function giveClover(groupId, receiverId, cloverCount, message) {
+
+    console.log("🟡 [giveClover] 전달된 groupId =", groupId);
+    console.log("🟡 [giveClover] 전달된 receiverId =", receiverId);
+    console.log("🟡 [giveClover] 전달된 cloverCount =", cloverCount);
+    console.log("🟡 [giveClover] 전달된 message =", message);
+
     const { data, error } = await supabase
-    .from('clovers')
-    .insert([{
-        group_id: groupId,
-        receiver_id: receiverId,
-        clover_count: cloverCount,
-        message: message
-    }])
+        .from('clovers')
+        .insert([{
+            group_id: groupId,
+            receiver_id: receiverId,
+            clover_count: cloverCount,
+            message: message
+        }])
 
     if (error) {
         console.error('칭찬 주기 실패:', error);
@@ -20,5 +26,6 @@ export async function giveClover(groupId, receiverId, cloverCount, message) {
         return { success: true, message: '하트 주기 성공' }
     }
 }
+
 
 export { supabase }
